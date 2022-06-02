a90fcf6da03e39a75faa81593b3d61315ad590eb
Omar Slik <omar.sleek@hotmail.com>
2022-06-02 13:46:01 +0200

    data

--- src/components/LogIn/LogIn-component.js
+++ src/components/LogIn/LogIn-component.js
@@ -1,41 +1,69 @@
 import { useEffect, useState } from "react";
 import "./Login.css";
-import { Message } from "./Message"; 
+import { Message } from "./Message";
 import { Alert } from "antd";
 import { useMediaQuery } from "react-responsive";
 
 const Login = (props) => {
-    const [password, setPassword] = useState(true);
-    function PasswordShow() {
-        password ? setPassword(false) : setPassword(true);       
-    }
-
-    //-----------------------------------------------------------------------
+  const [response, setResponse] = useState({});
+  const [password, setPassword] = useState(true);
+  function PasswordShow() {
+    password ? setPassword(false) : setPassword(true);
+  }
+  const chklogin = (e) => {
+    e.preventDefault();
+    let loginObject = {
+      userInput: e.target[0].value,
+      password: e.target[1].value,
+    };
+    sessionStorage.setItem("UserInput", e.target[0].value);
+    fetch("https://localhost:44332/User/Login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginObject),
+    }).then((response) => setResponse(response));
+  };
+  const chksignup = (e) => {
+    e.preventDefault();
+    const newUser = {
+      Email: e.target[0].value,
+      UserName: e.target[1].value,
+      Password: e.target[2].value,
+      FirstName: e.target[3].value,
+      LastName: e.target[4].value,
+    };
+    fetch("https://localhost:44332/User/Registration", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUser),
+    }).then((response) => setResponse(response));
+  };
+  //-----------------------------------------------------------------------
   return (
-<div className="main">
-    <Message/>
-    <input type="checkbox" id="chk" name="chk" aria-hidden="true"/>
-        <div className="login">
-            <form id="loginForm">
+    <div className="main">
+      <Message response={response} />
+      <input type="checkbox" id="chk" name="chk" aria-hidden="true" />
+      <div className="login">
+            <form id="loginForm" onsubmit={chklogin}>
                 <label for="chk" aria-hidden="true">Login</label>
                 <input id="userNameInput" type="text" placeholder="Username" minlength="2"  maxlength="50" required/>
                 <input id="passwordInput" type= {password ? "password" : "text"} name="pswd" placeholder="Password" minlength="2"  maxlength="50" required/>
                 <input type="checkbox" onclick={PasswordShow}/>
-                <button id="loginSubmit">Login</button>
+                <button id="loginSubmit" type="submit">Login</button>
             </form>
         </div>
         <div className="signup">
-    <form id="registration-form">
-        <label for="chk" aria-hidden="true">Sign up</label>
-        <input type="string" id="userEmail" placeholder="Email" minlength="1" maxlength="50" required/>
-        <input type="string" id="username" placeholder="Username" step="1" min="0" required/>
-        <input type="password" id="userPassword" placeholder="Password" maxlength="50" required/>
-        <input type="string" id="userFirstname" placeholder="Firstname" maxlength="50" required/>
-        <input type="string" id="userLastname" placeholder="Lastname" maxlength="50" required/>
-        <button>Sign up</button>
-    </form>
-</div>
-</div>
+        <form id="registration-form" onsubmit={chksignup}>
+            <label for="chk" aria-hidden="true">Sign up</label>
+            <input type="string" id="userEmail" placeholder="Email" minlength="1" maxlength="50" required/>
+            <input type="string" id="username" placeholder="Username" step="1" min="0" required/>
+            <input type="password" id="userPassword" placeholder="Password" maxlength="50" required/>
+            <input type="string" id="userFirstname" placeholder="Firstname" maxlength="50" required/>
+            <input type="string" id="userLastname" placeholder="Lastname" maxlength="50" required/>
+            <button  type="submit">Sign up</button>
+        </form>
+      </div>
+    </div>
   );
 };
 export default Login;
--- src/components/LogIn/message.js
+++ src/components/LogIn/message.js
@@ -1,45 +1,45 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Login.css";
 import { Alert } from "antd";
 const Message = (props) => {
+  const response = props.response;
   const [visibility, setVisibility] = useState(false);
-  const [response, setResponse] = useState(false);
+  const [responsestatus, setResponsestatus] = useState(false);
   const [isANumber, setIsANumber] = useState(true);
   const [responseMessage, setResponseMessage] = useState("");
 
-  function chkInput(response) {
-    setVisibility(true);
-    if (response.status === 200) {
-      setResponse(true);
-      response.text().then((data) => {
-        if (isNaN(data)) {
-          setIsANumber(false);
-          setResponseMessage(data);
-          setTimeout(function () {
-            window.location.reload();
-          }, 2000);
-        } else {
-          sessionStorage.setItem("UserId", data);
-          setTimeout(() => {
-            app.innerHTML = "";
-            render(app);
-          }, 1000);
-          setTimeout(function () {
-            window.location.href = "../index.html";
-          }, 2000);
-        }
-      });
-    } else {
-      response.text().then((response) => setResponseMessage(response));
-    }
-    setTimeout(function () {
-      setVisibility(false);
-    }, 3000);
+  setVisibility(true);
+  if (response.status === 200) {
+    setResponsestatus(true);
+    response.text().then((data) => {
+      if (isNaN(data)) {
+        setIsANumber(false);
+        setResponseMessage(data);
+        setTimeout(function () {
+          window.location.reload();
+        }, 2000);
+      } else {
+        sessionStorage.setItem("UserId", data);
+        setTimeout(() => {
+          app.innerHTML = "";
+          render(app);
+        }, 1000);
+        setTimeout(function () {
+          window.location.href = "../index.html";
+        }, 2000);
+      }
+    });
+  } else {
+    response.text().then((response) => setResponseMessage(response));
   }
+  setTimeout(function () {
+    setVisibility(false);
+  }, 3000);
+
   return (
     <>
       {visibility ? (
-        response ? (
+        responsestatus ? (
           isANumber ? (
             <Alert message="Login successful..." type="success" />
           ) : (
