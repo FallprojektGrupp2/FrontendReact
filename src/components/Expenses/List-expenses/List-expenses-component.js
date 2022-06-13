2ee986d962bf01d656016323aa2860bc43713355
MathiasAngelin <mathias.angelin@gmail.com>
2022-06-13 11:11:06 +0200

    it works!

--- src/components/Expenses/List-expenses/List-expenses-component.js
+++ src/components/Expenses/List-expenses/List-expenses-component.js
@@ -5,24 +5,28 @@ import { useEffect, useState } from "react";
 export function ListExpenses () {
 
     const [data, setdata] = useState([]);
-  
 
+    
     useEffect(() => {
-       setdata(GetExpenses());
+     let mydata = (GetExpenses())
+     .then(mydata => setdata(mydata))
     },[])
 
-    console.log(data)
+    if (data)
+    {
+        
+    }
 
-   
+  
 
     return (
         <div>
-       {/* {data.map((expenses)=> {
+       {data.map((expenses)=> {
            console.log(expenses)
            return <div key={expenses.ExpenseId}> {expenses.comment} </div>
-       })} */}
+       })}
      
-       <p>hej</p>
+      <p>hej</p>
        </div>
     )
 }
