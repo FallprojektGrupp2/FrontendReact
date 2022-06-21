5e842afe049d9b8c9e64235bd022bb722154c3c4
Emma Johansson <emmajohansson266@gmail.com>
2022-06-21 12:12:07 +0200

    Budget-diagram more readable

--- src/components/Budget/Budget-diagram/budget-histogram-component.js
+++ src/components/Budget/Budget-diagram/budget-histogram-component.js
@@ -1,45 +1,66 @@
 import Chart from 'react-google-charts'
 import './budget-diagram.css'
 //Primary color antD: #1890FF
-const topsum = 3500;
-const spentOnFood = 2457;
-const spentOnTransport = 1572;
-const spentOnShopping = 453;
-const spentOnEnjoyment = 576;
-const spentOnHome = 893;
-const spentOnOther = 523;
+const topsum = 3500; // set to highets spent if higher than highest category topsum in budget
+const foodBudget = [3000, 2457];
+const transportBudget = [1500, 1765];
+const shoppingBudget = [1000, 453];
+const entertainmentBudget = [1250, 576];
+const homeBudget = [1200, 893];
+const otherBudget = [2000, 523];
+
+const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const columnColors = ['#1890FF', '#FF7C00', '#FF0000']
+
+const d = new Date();
+document.write("The current month is " + months[d.getMonth()]);
+
+const percentage = (partialValue, totalValue) => {
+    const result = (100 * partialValue) / totalValue;
+    return Math.round(result).toString() + "%"
+}
 
 export const BudgetHistogramComponent = () => {
 
     return (
         <div className="budgetDiagramDiv">
+            <div className="monthlyBudgetDiv">
+                <h1>Budget {months[new Date().getMonth()]}</h1>
+            </div>
             <Chart
                 chartType="ColumnChart"
                 options={{
-                    colors: ['#1890FF'], backgroundColor: "#FFFFFF",
+                    colors: ['#e3e3e3'], backgroundColor: "#FFFFFF",
                     series: {
                         0: { targetAxisIndex: 0 }
                     },
                     chartArea: { height: '80%' },
                     title: "Monthly budget",
-                    hAxis: { title: "Categories", viewWindow: { min: 0, max: 6 } },
+                    hAxis: { title: "Categories", viewWindow: { min: 0, max: 6, }, },
                     vAxis: { title: "Sum", viewWindow: { min: 0, max: topsum } },
-                    legend: "none"
+                    legend: "none",
+                    allowHtml: true,
+                    stackSeries: true,
+                    isStacked: true
+
                 }}
                 style={{
                     'height': '600px',
                     'display': 'flex',
                     'justify-content': 'center',
-                    'minWidth': ' 900px'
+                    'minWidth': '1300px'
                 }}
                 height="600px"
-                data={[["Categories", "Sum"],
-                ["Food", spentOnFood],
-                ["Transportation", spentOnTransport],
-                ["Shopping", spentOnShopping],
-                ["Entertainment", spentOnEnjoyment],
-                ["Housing & Utilities", spentOnHome],
-                ["Miscellaneous", spentOnOther]]}
+                data={[["Categories", "Sum spent", { role: "style" }, { role: "annotation" }, "Sum left in budget"],
+                ["Food", foodBudget[1], columnColors[1], percentage(foodBudget[1], foodBudget[0]), foodBudget[0] - foodBudget[1]], //Categoryname, sum spent in category, status color sum spent vs maximum sum in category
+                ["Transportation", transportBudget[1], columnColors[2], percentage(transportBudget[1], transportBudget[0]), transportBudget[0] - transportBudget[1]],
+                ["Shopping", shoppingBudget[1], columnColors[1], percentage(shoppingBudget[1], shoppingBudget[0]), shoppingBudget[0] - shoppingBudget[1]],
+                ["Entertainment", entertainmentBudget[1], columnColors[0], percentage(entertainmentBudget[1], entertainmentBudget[0]), entertainmentBudget[0] - entertainmentBudget[1]],
+                ["Housing & Utilities", homeBudget[1], columnColors[1], percentage(homeBudget[1], homeBudget[0]), homeBudget[0] - homeBudget[1]],
+                ["Miscellaneous", otherBudget[1], columnColors[0], percentage(otherBudget[1], otherBudget[0]), otherBudget[0] - otherBudget[1]]]}
             />
         </div>
     );
