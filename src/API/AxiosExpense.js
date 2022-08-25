cfc22e375e9952188c6c59cc7e7a032ddf0de29b
MathiasAngelin <mathias.angelin@gmail.com>
2022-08-25 10:23:50 +0200

    Axiosexpense

--- src/API/AxiosExpense.js
+++ src/API/AxiosExpense.js
@@ -1,18 +1,40 @@
 import axios from "axios";
 
 
+
 export function GetExpenses ()  {
     
     const parsedToken = JSON.parse(localStorage.getItem('token'));
-
     return(
-    axios.get('https://localhost:44332/Expense/1')            
+    axios.get('https://localhost:44332/Expense/'+parsedToken)            
     .then((response) => {
         const data = response.data;
         return(
             data
         )
     })
+    )
+}
+
+export function CreateExpense(data) {
+
+    const parsedToken = JSON.parse(localStorage.getItem('token'));
+
+    const expenseForm = new FormData();
+    expenseForm.append(data.amount, "amount")
+    expenseForm.append(data.reciever, "reciever")
+    expenseForm.append(data.timeStamp, "timeStamp")
+    expenseForm.append(parsedToken, "userId")
+    expenseForm.append(data.categoryName, "categoryName")
+    
+
+    return(
+        axios({
+            method: "post",
+            url: 'https://localhost:44332/Expense/', 
+            data: expenseForm,
+        })
+
     )
 }
 
