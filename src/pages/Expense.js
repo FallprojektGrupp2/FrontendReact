1a97e0b851211c6a7b42387bfa21547246c20c4f
MathiasAngelin <mathias.angelin@gmail.com>
2022-09-22 12:01:48 +0200

    push

--- src/pages/Expense.js
+++ src/pages/Expense.js
@@ -1,15 +1,40 @@
 import { ListExpenses } from "../components/Expenses/List-expenses/List-expenses-component";
 import { CreateExpenseForm } from "../components/Expenses/Create-expense/Create-expense-component";
 import { PieChartComponent } from "../components/Expenses/List-expenses/Expenses-pie-chart";
+import axios from "axios";
+
+
+//skicka event i createexpenseform
+//fetchen av listan av expenses bör ligga här
+//redux 
+
+//axiosanrop här, skicka det till listExpense i ett state
+//listexpense ta emot en prop
+
+//Createexpense berättar 
+//Callbackfun
 
 
 const Expense = () => {
+    const [expenses, setExpenses] = []
+    
+    let handleNewExpenseForm = () => {
+        axios.get('https://localhost:44332/Expense/'+parsedToken)            
+    .then((response) => {
+        const data = response.data;
+        return(
+            data
+        )
+    })
+    
+
+    }
     return (
         <>
         <h1>Expenses</h1>
         <div className="ExpensePage">
-            <ListExpenses />
-            <CreateExpenseForm />
+            <ListExpenses expenses={expenses}/>
+            <CreateExpenseForm handleSubmit={handleNewExpenseForm}/> 
         </div>
         <div className="PieChart">
         <PieChartComponent/>
