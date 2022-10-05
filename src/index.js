27b2341982f7ee98f0d81e2bc471917d25dc3ea7
Leila Hafarian <leila.hafarian@gmail.com>
2022-10-05 18:46:46 +0200

    new

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
+          <Route path="/" element={<App />}>
           <Route path="/" element={<Home />} />
           <Route path="/Expense" element={<Expense />} />
           <Route path="/Features" element={<Features/>}/>
