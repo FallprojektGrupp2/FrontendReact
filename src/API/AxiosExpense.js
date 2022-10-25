f141ae9928e66cd0e1b55f21d03eb848071fe54e
Emma Johansson <emmajohansson266@gmail.com>
2022-10-25 10:49:16 +0200

    Fixed sums spent line in expense statistics

--- src/API/AxiosExpense.js
+++ src/API/AxiosExpense.js
@@ -3,72 +3,85 @@ import axios from "axios";
 
 
 
-export function GetExpenses ()  {
+export function GetExpenses() {
   const parsedToken = JSON.parse(localStorage.getItem('token'));
-  
-    return(
-    axios.get('https://localhost:44332/Expense/'+parsedToken)         
-    .then((response) => {
-      console.log("GetExpenses done")
+
+  return (
+    axios.get('https://localhost:44332/Expense/' + parsedToken)
+      .then((response) => {
+        console.log("GetExpenses done")
         const data = response.data;
-        return(
-            data
-            
+        return (
+          data
+
         )
-    })
-    )
+      })
+  )
 }
 
 
-export const GetSum = () => { 
+export const GetSum = () => {
 
   const parsedToken = JSON.parse(localStorage.getItem('token'));
 
-  return(
-    axios.get('https://localhost:44332/PieChart/'+parsedToken)
-    .then((res )=>{
+  return (
+    axios.get('https://localhost:44332/PieChart/' + parsedToken)
+      .then((res) => {
         const data = res.data;
         return data
-    })
-  )  
+      })
+  )
+}
+
+
+export const GetSumSpentYearAndMonth = () => {
+  const parsedToken = JSON.parse(localStorage.getItem('token'));
+
+  return (
+    axios.get('https://localhost:44332/Expense/TotalSumsSpent/' + parsedToken)
+      .then((res) => {
+        const data = res.data;
+        return data
+      })
+  )
 }
 
 
 export const CreateExpense = async (formData) => {
   const parsedToken = JSON.parse(localStorage.getItem('token'));
-      await axios({
+  await axios({
 
-              method: "post",
-              url: 'https://localhost:44332/Expense',
-              data: {
-                  "amount": formData.amount,
-                  "receiver": formData.receiver,
-                  "timeStamp": formData.timeStamp,
-                  "comment": formData.comment,
-                  "userId": formData.userId,
-                  "categoryName": formData.categoryName
-                },
-              headers: { "Content-Type": "application/json" },
-            })
-        }
-          
-
-
-
-
-    
-export function DeleteExpenses (id)  {
-    
-  const parsedToken = JSON.stringify(id)
-  return(
-  axios.delete('https://localhost:44332/Expense/'+id)         
-  .then((response) => {
-    debugger
-      const data = response.data;
-      return(
-          data
-          
-      )
+    method: "post",
+    url: 'https://localhost:44332/Expense',
+    data: {
+      "amount": formData.amount,
+      "receiver": formData.receiver,
+      "timeStamp": formData.timeStamp,
+      "comment": formData.comment,
+      "userId": formData.userId,
+      "categoryName": formData.categoryName
+    },
+    headers: { "Content-Type": "application/json" },
   })
+}
+
+
+
+
+
+
+export function DeleteExpenses(id) {
+
+  const parsedToken = JSON.stringify(id)
+  return (
+    axios.delete('https://localhost:44332/Expense/' + id)
+      .then((response) => {
+        debugger
+        const data = response.data;
+        return (
+          data
+
+        )
+      })
   )
 }
